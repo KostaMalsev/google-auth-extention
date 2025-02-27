9aaa804ac021d04dc3be159fad5b600f812ccbeb
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2025-02-27 17:45:22 +0200

    Update ItemsList.js

--- ItemsList.js
+++ ItemsList.js
@@ -1,2 +1,22 @@
+import React from 'react';
+import '../styles/components.css';
 
-
+const ItemsList = ({ items }) => {
+  return (
+    <div className="items-list-container">
+      <h2>Your Items</h2>
+      <ul className="items-list">
+        {items.map(item => (
+          <li key={item.id} className="item">
+            <div className="item-name">{item.name}</div>
+            {item.description && (
+              <div className="item-description">{item.description}</div>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemsList;
