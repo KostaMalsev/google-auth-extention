c7d52c446f0496b3b43259557da8933fb399b50d
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2025-02-27 17:44:54 +0200

    Update UserProfile.js

--- UserProfile.js
+++ UserProfile.js
@@ -1,2 +1,27 @@
+import React from 'react';
+import '../styles/components.css';
 
-
+const UserProfile = ({ user, onSignOut }) => {
+  if (!user) return null;
+
+  return (
+    <div className="user-profile">
+      {user.picture && (
+        <img 
+          src={user.picture} 
+          alt={user.name || 'User'} 
+          className="user-avatar" 
+        />
+      )}
+      <div className="user-info">
+        <div className="user-name">{user.name}</div>
+        <div className="user-email">{user.email}</div>
+      </div>
+      <button className="sign-out-button" onClick={onSignOut}>
+        Sign Out
+      </button>
+    </div>
+  );
+};
+
+export default UserProfile;
